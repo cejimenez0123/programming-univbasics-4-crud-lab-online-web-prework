c23f6cbf2a24b2c866d2a3ef8f1afad3f1666612
Christian Jimenez <christianjimenez0123@gmail.com>
2019-10-16 21:47:44 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -29,12 +29,12 @@ end
 
 def retrieve_element_from_index(array, index_number)
 retrieve_element_from_index = ["wow", "I", "am", "really", "learning", "arrays!"]
-retrieve_element_from_index = [1]
+retrieve_element_from_index = [2]
 end
 
 def retrieve_first_element_from_array(array)
 retrieve_first_element_from_array = ["wow", "I", "am", "really", "learning", "arrays!"]
-retrieve_first_element_from_array = ["wow"]
+retrieve_first_element = [0]
 end
 
 def retrieve_last_element_from_array(array)
